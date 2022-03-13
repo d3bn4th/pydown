4babf25ad4a950412eb7e69ff1a8ec9365fcfcbd
d3bn4th <d3bn4th@gmail.com>
2022-03-13 17:39:45 +0530

    'delete feature added'

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
+from ast import Return
 from pytube import YouTube
+import os
 
 link = input("Enter the link: ")
 yt = YouTube(link)
+type = int(input("Audio - 1 or Audio/Video - 2 : "))
 
-
-'''
-# Title of video
-print("Title: ", yt.title)
-
-# Number of Views of video
-print("Number of views: ", yt.views)
-
-# Description of video
-print("Description of the video: ", yt.description)
-
-# Rating
-print("Ratings: ", yt.rating)
-
-# printing all the available streams
-print(yt.streams.filter(progressive = True))
-'''
-type = int(input("Audio - A or Video - V : "))
-
+# for desktop
 def down(type):
     if type == 1:
         # To get the highest resolution streams
-        ys = yt.streams.get_highest_resolution()
+        ys = yt.streams.filter(only_audio=True)
         print("Downloading......")
-        ys.download(r'C:\Users\thicc\Student Dropbox (Old)\Arihant Debnath\My PC (Thinkpad)\Desktop')
+        ys[0].download()
         print("Download completed!!")
 
     if type == 2:
         # To get the highest resolution streams
-        yt.streams.get_highest_resolution().download()
+        ys = yt.streams.get_highest_resolution()
         print("Downloading......")
-        ys.download(r'C:\Users\thicc\Student Dropbox (Old)\Arihant Debnath\My PC (Thinkpad)\Desktop')
+        ys.download()
         print("Download completed!!")
+    if type == 3:
+        return
 
 down(type)
+
+file_title = yt.title
+print(file_title)
+
+# getting the current working directory
+cwd = os.getcwd()
+file_location = cwd +'\\'+file_title+'.mp4'
+print(file_location)
+os.startfile(file_location)
+
+delete_file = input("Do you want to delete the file(YES) \n>> ")
+if delete_file == "YES":
+    os.remove(file_location)
+    print("File "+file_title+" Deleted")
+if delete_file == "NO":
+    print("File" + file_title+ "Saved \n" + "Location: "+file_location)
 
